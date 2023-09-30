8c0a4876d76f531c8dd1d5bed9936b5bf3ef84c6
local user <user@mil8a-118b-03.mmcs.sfedu.ru>
2023-09-30 09:39:58 +0300

    Asap

---
+++ time.pas
@@ -0,0 +1,15 @@
+﻿begin
+  var year:= ReadInteger('Введите год:');
+  if year mod 4=0 then
+    if(year mod 100 = 0) and (year mod 400 <>0) then
+      year:=365
+    else
+    year:=366
+   else
+     year:=365;
+   if year = 365 then
+     writeLn('Это невисокосный год')
+   else
+     WriteLn('Это високосный год');
+   WriteLn($'Количество дней:{year}');
+end.
